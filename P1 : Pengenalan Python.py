90da187ac2aebddd345b92dd4e2e234b5cc13886
dina <39585270+dinafadila@users.noreply.github.com>
2019-08-22 17:08:06 +0700

    Create P1 : Pengenalan Python.py

---
+++ P1 : Pengenalan Python.py
@@ -0,0 +1,10 @@
+kondisi = str(input())
+
+if kondisi == HUJAN:
+    print("Anda boleh membawa jenis dan warna payung apa saja")
+    
+elif kondisi == TIDAK HUJAN:
+    print("Total harga yang harus dibayarkan : ", int(bayar))
+    
+else:
+    print("Total harga yang harus dibayarkan : ", int(total_harga))
